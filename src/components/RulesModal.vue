274dbedca2218e8550bfb92e01621754bfd41742
unknown <mlukanus@gmail.com>
2021-01-18 11:19:29 -0600

    Adds TODO for RulesModal

---
+++ src/components/RulesModal.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <b-button v-b-modal.modal-1 variant="secondary">Rules</b-button>
+
+        <b-modal
+            ok-only
+            ok-variant="success"
+            id="modal-1" 
+            title="Rules"
+        >
+            <p>TODO</p>
+        </b-modal>
+    </div>
+</template>
